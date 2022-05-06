ac0aca819301398fd7313d0cf68034b12c7cc0ee
Sam Button <sam_button_94@hotmail.co.uk>
2022-05-06 16:22:53 +0100

    Step 22: added web helper

--- spec/feature-tests/hit_points_spec.rb
+++ spec/feature-tests/hit_points_spec.rb
@@ -1,10 +1,7 @@
 feature "implementing hit points" do
 
   scenario 'see player 2s hit points' do
-    visit("/")
-    fill_in :player_1, with: 'Slava'
-    fill_in :player_2, with: 'Sam'
-    click_on 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Sam: HP 100'
   end
 
--- spec/feature-tests/play_feature_spec.rb
+++ spec/feature-tests/play_feature_spec.rb
@@ -1,11 +1,8 @@
 feature "Playing the game" do
 
   scenario "page contains 'Testing infrastructure working!'" do
-    visit("/")
-    fill_in :player_1, with: 'Tom'
-    fill_in :player_2, with: 'Sam'
-    click_on 'Submit'
-    expect(page).to have_content 'Tom vs Sam'
+    sign_in_and_play
+    expect(page).to have_content 'Slava vs Sam'
   end
 
 end
